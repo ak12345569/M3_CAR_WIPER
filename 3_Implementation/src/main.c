88c3874d4eeb1bde4f94054083682bbc3edc7b21
ak12345569 <101049933+ak12345569@users.noreply.github.com>
2022-05-13 14:09:07 +0530

    Add files via upload

---
+++ 3_Implementation/src/main.c
@@ -0,0 +1,22 @@
+#include <stdint.h>
+#define ADC_BASE_ADDR               0X40012000UL
+#define ADC_CR1_REG_OFFSET          0X04UL
+#define ADC_CR1_REG_ADDR             (ADC_BASE_ADDR + ADC_CR1_REG_OFFSET)
+
+#define RCC_BASE_ADDR                 0X4002300UL
+#define RCC_APB2_ENR_OFFSET           0X44UL
+#define RCC_APB2_ENR_ADDR             (RCC_BASE_ADDR + RCC_APB2_ENR_OFFSET)
+
+int main(void)
+
+{
+   uint32_t *pRccApb2Enr = (uint32_t*) RCC_APB2_ENR_ADDR;
+
+   uint32_t *pAdcCr1Reg = (uint32_t*) ADC_CR1_REG_ADDR;
+
+   *pRccApb2Enr |= (1<<8);
+
+   *pAdcCr1Reg |=(1<<8);
+
+    for(;;);
+}
